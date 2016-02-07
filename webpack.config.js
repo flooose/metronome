7790b8393dadb65825696c8383f1529507c49e7c
christopher floess <skeptikos@gmail.com>
2016-02-07 10:19:05 +0100

    configure eslint

--- webpack.config.js
+++ webpack.config.js
@@ -11,9 +11,9 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
-          presets: ['react','es2015']
-        }
-      }
-    ]
+          presets: ['react','es2015'],
+        },
+      },
+    ],
   },
 }
